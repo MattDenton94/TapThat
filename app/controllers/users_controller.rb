77c439b0d47b10e6ea3afb7e5b95d2679d4d996e
Matthew Denton <mattdenton94@gmail.com>
2025-01-11 12:27:41 +0200

    fixing migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  def show
+    @posts = @user.posts
+    @followers = @user.followers
+    @following = @user.followed_users
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  def follow
+    current_user.follow(@user)
+    redirect_to @user, notice: "You are now following #{@user.username}."
+  end
+
+  def unfollow
+    current_user.unfollow(@user)
+    redirect_to @user, notice: "You have unfollowed #{@user.username}."
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+     @user.destroy
+    redirect_to root_path, notice: "Your account has been deleted."
+  end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :bio, :email, :password, :password_confirmation)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Follow.destroy_all
+Share.destroy_all
+Like.destroy_all
+Comment.destroy_all
+UserInterest.destroy_all
+Post.destroy_all
+Interest.destroy_all
+User.destroy_all
+# Create some users with unique emails and passwords
+user1 = User.create!(username: 'john_doe', first_name: 'John', last_name: 'Doe', bio: 'Lover of tech and photography.', email: 'john@example.com', password: 'password123')
+user2 = User.create!(username: 'jane_smith', first_name: 'Jane', last_name: 'Smith', bio: 'Adventurer and traveler.', email: 'jane@example.com', password: 'password123')
+
+# Create some interests with unique titles
+interest1 = Interest.create!(title: 'Photography')
+interest2 = Interest.create!(title: 'Traveling')
+interest3 = Interest.create!(title: 'Coding')
+
+# Create user interests (associating users with interests)
+UserInterest.create!(user_id: user1.id, interest_id: interest1.id)
+UserInterest.create!(user_id: user1.id, interest_id: interest2.id)
+UserInterest.create!(user_id: user2.id, interest_id: interest3.id)
+
+# Create some posts
+post1 = Post.create!(user: user1, content: 'Just took some amazing photos of the sunset!')
+post2 = Post.create!(user: user2, content: 'Exploring the world one city at a time.')
+
+# Create likes
+Like.create!(user: user1, post: post2)
+Like.create!(user: user2, post: post1)
+
+# Create shares
+Share.create!(post_id: post1.id)
+Share.create!(post_id: post2.id)
+
+# Create some comments
+Comment.create!(user: user1, post: post2, content: 'Such an inspiring post!')
+Comment.create!(user: user2, post: post1, content: 'Amazing photos, I want to learn photography!')
+
+# Create followers (for the follow relationship)
+Follow.create!(follower_id: user1.id, followed_id: user2.id)
+Follow.create!(follower_id: user2.id, followed_id: user1.id)
+
+puts "Seed data created successfully."
