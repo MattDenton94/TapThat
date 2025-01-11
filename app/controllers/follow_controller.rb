9d5cf60ac943b700b459f1e2f9cc8607c71faaee
Simon Luckhoff <simonluckhoff@gmail.com>
2025-01-11 14:26:53 +0200

    follower

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,10 +1,15 @@
 class FollowController < ApplicationController
   before_action :authenticate_user!
 
+  # following someone
   def create
-    @follow = Follow.new(follow_params)
-    @follow.save
-    redirect_to follow_path(@follow)
+    @user_to_follow = User.find(params[:user_id])
+
+    if current_user == @user_to_follow
+      redirect_to @user_to_follow, alert: "You can't follow yourself"
+      return
+    end
+    
   end
-  
+
 end
