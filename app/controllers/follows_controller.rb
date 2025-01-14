ba2ffd3d49526e079b0d71123134cb8252af045f
Simon Luckhoff <simonluckhoff@gmail.com>
2025-01-14 18:54:12 +0200

    follower

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,16 +1,31 @@
 class FollowsController < ApplicationController
   before_action :authenticate_user!
 
-  # to follow someone
+  # Display users you're following
+  def following
+    @following = current_user.following
+  end
+
+  # Display users who follow you
+  def followers
+    @followers = current_user.followers
+  end
+
   def create
-    user = User.find(params[:followed_id])
-    current_user.following << user
-    redirect_to user_path(user), notice: "successfully following"
+    @user_to_follow = User.find(params[:id])
+    if current_user.follow(@user_to_follow)
+      render json: { following: true }, status: :ok
+    else
+      render json: { error: "Unable to follow user" }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    user = User.find(params[:id])
-    current_user.follwing.delete(user)
-    redirect_to user_path(user), notice: "successfully unfollowing"
+    @user_to_unfollow = User.find(params[:id])
+    if current_user.unfollow(@user_to_unfollow)
+      render json: { following: false }, status: :ok
+    else
+      render json: { error: "Unable to unfollow user" }, status: :unprocessable_entity
+    end
   end
 end
