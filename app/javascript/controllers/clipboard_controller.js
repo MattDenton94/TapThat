bb6a510c7945a13e5ff8d4ffeaa1b4eca0454a1f
Simon Luckhoff <simonluckhoff@gmail.com>
2025-01-16 08:00:25 +0200

    copy to clipboard function

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -4,7 +4,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = [ "source" ]
+  static values = { url:String }
   copy() {
-    navigator.clipboard.writeText(this.sourceTarget.value)
+    navigator.clipboard.writeText(this.urlValue)
   }
 }
